8042c9d8f402a15f43182b5a30a6dc853676056e
Almighty-Zinger <bennettdblodgett@gmail.com>
2024-04-08 10:51:06 -0700

    Top down

---
+++ In The Red Rework/Assets/Dragger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragger : MonoBehaviour
+{
+    private bool dragging;
+    
+    void Update() {
+if(!dragging) return;
+    
+ 
+ transform.position;
+    }
+    
+    void OnMouseDown () {
+dragging = true;
+}
+
+
+
+}
