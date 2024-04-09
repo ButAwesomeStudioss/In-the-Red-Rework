fe56b7dae8e8b0d43daf8fe216ac9e5a697c57e8
ButAwesomeStudioss <150070914+ButAwesomeStudioss@users.noreply.github.com>
2024-04-09 10:48:38 -0700

    Not Finished Timer

--- In The Red Rework/Assets/Day TImer.cs
+++ In The Red Rework/Assets/Day TImer.cs
@@ -1,9 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
 
 public class DayTImer : MonoBehaviour
 {
+    private TMP_Text _Timer;
+    enum TimerType {Countdown, Stopwatch }
+    [SerializeField] private TimerType timerType;
+
+[SerializeField] private float timeToDisplay = 60.00f;
+
+    private bool _isRunning;
+
+    private void Awake()
+    {
+        _Timer = GetComponent<TMP_Text>();
+
+    }
+
+    private void OnEnable()
+    {
+        EventManager.TimerStart += EventManager_TimerStart;
+        EventManager.TimerStop += EventManager_TimerStop;
+        EventManager.TimerUpdate += EventManager_TimerUpdate;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.TimerStart -= EventManager_TimerStart;
+        EventManager.TimerStop -= EventManager_TimerStop;
+        EventManager.TimerUpdate -= EventManager_TimerUpdate;
+    }
+
+
+    private void EventManager_TimerUpdate(float value) => timeToDisplay += value;
+   
+
+
+    private void EventManager_TimerStop() => _isRunning = false;
+    
+
+
+    private void EventManager_TimerStart() => _isRunning = true;
+   
+
     // Start is called before the first frame update
     void Start()
     {
