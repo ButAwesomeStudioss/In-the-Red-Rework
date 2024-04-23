40187dee24a698de2471fc89b56967fdf08c1144
ButAwesomeStudioss <150070914+ButAwesomeStudioss@users.noreply.github.com>
2024-04-23 10:28:24 -0700

    Revert "Update Tile.cs"
    This reverts commit 7dca5a37af95f6e9ffa96adea7c6cfe025f586cc.

--- In The Red Rework/Assets/Scripts/Tile.cs
+++ In The Red Rework/Assets/Scripts/Tile.cs
@@ -9,15 +9,7 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
-<<<<<<< Updated upstream
     public GameObject thing;
-=======
-<<<<<<< HEAD
-    [SerializeField] private GameObject _highlight;
-=======
-    public GameObject thing;
->>>>>>> 1ddf971667952cd04184371851d865877f51a319
->>>>>>> Stashed changes
 
     public void Init(bool isOffset)
     {
@@ -25,28 +17,10 @@ public class Tile : MonoBehaviour
 
     }
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-    void OnMouseEnter()
-    {
-        _highlight.SetActive(true);
-    }
-
-    void OnMouseExit()
-    {
-        _highlight.SetActive(false);
-    }
-=======
->>>>>>> Stashed changes
 private void OnMouseOver() {
                
  if(Input.GetMouseButtonDown(0))
  thing.transform.position = gameObject.transform.position;
-<<<<<<< Updated upstream
-=======
->>>>>>> 1ddf971667952cd04184371851d865877f51a319
->>>>>>> Stashed changes
 }
   
                
