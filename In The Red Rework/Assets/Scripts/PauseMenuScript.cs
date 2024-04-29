97b848615359627e05f73d3864f6dbffc5f1ba53
ThatGuyOnTheInternet <165066859+ThatGuyOnTheInternet@users.noreply.github.com>
2024-04-29 10:52:18 -0700

    Pause Beginnings

---
+++ In The Red Rework/Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+   public void Pause()
+    {
+
+    }
+    public void Home()
+    {
+
+    }
+    public void Resume()
+    {
+
+    }
+}
