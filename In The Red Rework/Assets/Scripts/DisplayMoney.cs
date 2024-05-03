9039554f8cbad94df5daa877f4df107df0228c52
ButAwesomeStudioss <150070914+ButAwesomeStudioss@users.noreply.github.com>
2024-05-03 10:15:33 -0700

    Revert "Scottleduc.jpeg"
    This reverts commit 4c713592972c66513d8d203d5128a68c54697452.

--- In The Red Rework/Assets/Scripts/DisplayMoney.cs
+++ In The Red Rework/Assets/Scripts/DisplayMoney.cs
@@ -5,14 +5,6 @@ public class DisplayMoney : MonoBehaviour
 {
     void Update()
     {
-<<<<<<< Updated upstream
 
-=======
-        if (other.tag == "CoinCount")
-        {
-            UiObject.SetActive(true);
-            gameObject.name = "100";
-        }
->>>>>>> Stashed changes
     }
 }
