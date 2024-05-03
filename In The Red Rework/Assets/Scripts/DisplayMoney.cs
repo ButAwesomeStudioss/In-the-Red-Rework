4c713592972c66513d8d203d5128a68c54697452
ButAwesomeStudioss <150070914+ButAwesomeStudioss@users.noreply.github.com>
2024-05-03 10:14:07 -0700

    Scottleduc.jpeg

--- In The Red Rework/Assets/Scripts/DisplayMoney.cs
+++ In The Red Rework/Assets/Scripts/DisplayMoney.cs
@@ -5,6 +5,14 @@ public class DisplayMoney : MonoBehaviour
 {
     void Update()
     {
+<<<<<<< Updated upstream
 
+=======
+        if (other.tag == "CoinCount")
+        {
+            UiObject.SetActive(true);
+            gameObject.name = "100";
+        }
+>>>>>>> Stashed changes
     }
 }
